735638c8ddbe12da2a2c33fa5d88b17b5d410279
AlexanderStoynov <alexstoynov999@abv.bg>
2025-01-10 17:53:45 +0200

    Tweaked layout and split project into layers.

--- CarDealerWebProject/Controllers/HomeController.cs
+++ CarDealerWebProject/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace CarDealerWebProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
